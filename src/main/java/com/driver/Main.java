5960eb07cfa90c78a8d5a0172b091e83e0e85457
lavanya-53 <lavanyay796@gmail.com>
2026-03-10 20:07:47 +0530

    Implement Product class with overloaded methods
    Added a Product class with overloaded product methods for different parameter types.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,38 @@
 package com.driver;
+ import java.util.*;
 
 public class Main {
 
+    static class Product {
+
+        public int product(int x, int y){
+            return x*y;
+        }
+
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        Product p = new Product();
+
+        System.out.println(p.product(x,y));
+        System.out.println(p.product(x,y,z));
+        System.out.println(p.product(a,b));
+    }
 }
