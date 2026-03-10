334b802666cf881df77cfb991bbe87288484cd41
lavanya-53 <lavanyay796@gmail.com>
2026-03-10 20:16:16 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,40 @@
+package com.driver;
+
+import java.util.*;
+
 public class Main {
 
     static class Product {
 
-        public int product(int x, int y){
-            return x*y;
+        public int product(int x, int y) {
+            return x * y;
         }
 
-        public int product(int x,int y,int z){
-            return x*y*z;
+        public int product(int x, int y, int z) {
+            return x * y * z;
         }
 
-        public double product(double x,double y){
-            return x*y;
+        
+        public double product(double x, double y) {
+            return x * y;
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
 
         Product p = new Product();
 
-        System.out.println(p.product(10,20));
-        System.out.println(p.product(10,20,30));
-        System.out.println(p.product(10.0,20.0));
+        System.out.println(p.product(x, y));
+        System.out.println(p.product(x, y, z));
+        System.out.println(p.product(a, b));
     }
 }
